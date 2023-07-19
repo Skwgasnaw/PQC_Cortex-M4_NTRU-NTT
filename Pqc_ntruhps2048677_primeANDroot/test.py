70db887b6616c302abc944a9aa1ef3285a6b3476
Chung <43467000+ChenChung107360206@users.noreply.github.com>
2023-07-19 19:48:42 +0800

    Update test.py

--- Pqc_ntruhps2048677_primeANDroot/test.py
+++ Pqc_ntruhps2048677_primeANDroot/test.py
@@ -1,11 +1,11 @@
 primeQ = 1419771931
-staticY = 1419771930
-y = pow(staticY, 1, primeQ)
+staticZ = 1419771930
+z = pow(staticZ, 1, primeQ)
 for i in range(0, 511):
-	y = pow(y * staticY, 1, primeQ)
-print("(y^512) mod primeQ =", y)
+	z = pow(z * staticZ, 1, primeQ)
+print("(z^512) mod primeQ =", z)
 
-staticZ = 683615825
-z = pow(staticZ, 2, primeQ) * staticZ
-z = pow(z, 1, primeQ) 
-print("(z^3) mod primeQ =", z)
+staticY = 683615825
+y = pow(staticY, 2, primeQ) * staticY
+y = pow(y, 1, primeQ) 
+print("(y^3) mod primeQ =", y)
